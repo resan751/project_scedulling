b8795f431f3a51d3f755b427dc15a71963c0e00a
resan <resansaputra2@gmail.com>
2026-05-06 16:04:56 +0700

    frontend update

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import AuthRouter from './routes/auth.route.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const port = 3000
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', AuthRouter)
 
